d5a33c4aeae5f7fe5af19a55c1c86f7eb324c8af
Shubham kumar <s.kumar.s8101997@gmail.com>
2024-03-13 11:22:33 +0530

    added swc helper

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -14,19 +14,19 @@ const ItemList = ({items,dummy})=>{
 
 
     return <div>
-        {items.map(item => <div key={item.card.info.id} className="p-2 m-2 border border-b-2 text-left flex">
+        {items?.map(item => <div key={item?.card?.info?.id} className="p-2 m-2 border border-b-2 text-left flex">
           <div className="w-9/12">
           <div className="py-2">
-                <span>{item.card.info.name}</span><br/>
-                <span>Rs. {item.card.info.price?item.card.info.price/100:item.card.info.defaultPrice/100}</span>
+                <span>{item?.card?.info?.name}</span><br/>
+                <span>Rs. {item?.card?.info?.price?item?.card?.info?.price/100:item?.card?.info?.defaultPrice/100}</span>
             </div>
-            <p className="text-xs">{item.card.info.description}</p>
+            <p className="text-xs">{item?.card?.info?.description}</p>
             </div>
             <div className="w-3/12 p-4">
             <div className="absolute">
                 <button className="p-2 mx-16 rounded-lg bg-black text-white shadow-lg" onClick={()=> handleAddItem(item)}>Add+</button>
             </div>
-                <img src={CDN_URL + item.card.info.imageId} />
+                <img src={CDN_URL + item?.card?.info?.imageId} />
             </div>
         </div>
         )}
